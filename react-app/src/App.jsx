b719ec00c407c2f7b916a027b19dc38bc7c957ca
Rebecca Aaland <reaaland@gmail.com>
2026-06-02 17:38:31 -0500

    adding structure for react

---
+++ react-app/src/App.jsx
@@ -0,0 +1,26 @@
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
+
+function App() {
+  return (
+    <div>
+      <h1>PawCircle</h1>
+      <p>Connecting pet owners and caregivers.</p>
+    </div>
+  );
+}
+
+export default App;
