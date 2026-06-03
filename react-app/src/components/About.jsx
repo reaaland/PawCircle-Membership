5e3fd7326244d452dfc040d3dcbe1c9728bade34
Rebecca Aaland <reaaland@gmail.com>
2026-06-02 22:45:18 -0500

    adding About serction to react

---
+++ react-app/src/components/About.jsx
@@ -0,0 +1,11 @@
+function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Welcome to Paw Circle Membership.</p>
+        </div>
+    )
+}
+
+export default About;
+ 
