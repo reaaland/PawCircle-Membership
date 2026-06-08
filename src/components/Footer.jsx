8497670cf1fc5b50b07b2e4697d0d2f3830427ff
Rebecca Aaland <reaaland@gmail.com>
2026-06-08 01:42:27 -0500

    changed a href to links in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/PC_Logo.png";
 
 function Footer() {
@@ -6,32 +7,42 @@ function Footer() {
       <div className="container">
         <div className="footer__content">
           <div>
-            <img src={logo} alt="PawCircle Logo" className="footer__logo" />
+            <img
+              src={logo}
+              alt="PawCircle Logo"
+              className="footer__logo"
+            />
+
+            <h4>
+              <span className="purple">PawCircle</span>
+            </h4>
+
             <p>
-              <span className="purple">PawCircle</span> helps pet owners and
-              pet caregivers connect directly in their community.
+              Helping pet owners and pet caregivers connect
+              directly within their communities.
             </p>
-          </div>
+            </div>
 
           <div className="footer__column">
             <h4>Explore</h4>
-            <a href="/">Home</a>
-            <a href="/services">Services</a>
-            <a href="/membership">Membership</a>
-            <a href="/join">Join PawCircle</a>
+            <Link to="/">Home</Link>
+            <Link to="/services">Services</Link>
+            <Link to="/membership">Membership</Link>
+            <Link to="/join">Join PawCircle</Link>
           </div>
 
           <div className="footer__column">
             <h4>Company</h4>
-            <a href="/about">About</a>
-            <a href="/contact">Contact</a>
-            <a href="/faq">FAQ</a>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/faq">FAQ</Link>
           </div>
 
           <div className="footer__column">
             <h4>Legal</h4>
-            <a href="/terms">Terms of Conduct</a>
-            <a href="/privacy">Privacy Policy</a>
+            <Link to="/terms">Terms of Use</Link>
+           <Link to="/privacy">Privacy Policy</Link>
+           <Link to="/code">Code of Conduct </Link>
           </div>
         </div>
 
